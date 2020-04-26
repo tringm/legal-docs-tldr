cc39c56f4a040bd8c77e35905a7c47dfada91fd7
Tri Nguyen <tringuyenminh23@gmail.com>
2020-04-26 22:58:30 +0300

    use wholename instead of name for find

--- scripts/download_tosdr_data.sh
+++ scripts/download_tosdr_data.sh
@@ -2,6 +2,7 @@
 
 function usage {
   echo "usage: $(basename "$0") [target_directory]
+
   download tosdr.org services rating data to the target_directory
   "
 }
@@ -19,7 +20,7 @@ if [ -d $1 ]
 then
   git clone https://github.com/tosdr/tosdr.org.git "$1"
   echo "removing all except the api folder"
-  find "$1" -maxdepth 1 ! -name "api" ! -name "$1" -exec rm -rf {} \;
+  find "$1" -maxdepth 1 ! -wholename "$1/api" ! -wholename "$1" -exec rm -rf {} \;
   echo "extracting all json files except all.json"
   find "$1/api" -name "*.json" ! -name "all.json" -exec mv {} "$1" \; && rm -rf "$1/api"
 else
