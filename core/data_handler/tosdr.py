d8998337bf000beeb49d5c0f5597e8c1b7056aec
Tri Nguyen <tringuyenminh23@gmail.com>
2020-08-25 15:21:58 +0300

    Add data class for tosdr service and summary case

--- core/data_handler/tosdr.py
+++ core/data_handler/tosdr.py
@@ -2,6 +2,7 @@ import json
 import logging
 from pathlib import Path
 from typing import Dict, List, Tuple, Optional
+from dataclasses import dataclass, field
 
 from core.utils.logging_utils import log_exec_time
 from bs4 import BeautifulSoup
@@ -10,7 +11,7 @@ LOADER_LOG = logging.getLogger("TosdrDataLoader")
 PROCESSOR_LOG = logging.getLogger("TosdrDataProcessor")
 
 
-class LoadingServiceDataError(Exception):
+class ServiceDataLoadError(Exception):
     def __init__(self, service_name, msg=''):
         super().__init__()
         self._service_name = service_name
@@ -24,99 +25,152 @@ class LoadingServiceDataError(Exception):
 class TosdrDataLoader:
     """Manage tosdr service data files"""
     def __init__(self):
-        self._loaded_services_data = {}
+        # TODO: Store file path and not load all services into memory
+        self._all_service_data = {}
+
+    def add_service_data(self, service_name: str, service_data: 'TosdrServiceData', force_reload=False):
+        """ Add a service data
+
+        Args:
+            service_name (str): name of the service
+            service_data (TosdrServiceData): service data
+            force_reload (bool, optional): reload the data file even if the service data has been loaded before, defaults to False
+
+        Returns:
+
+        """
+        if service_name in self._all_service_data and not force_reload:
+            LOADER_LOG.debug(f"{service_name} data existed and not force reload")
+        else:
+            self._all_service_data[service_name] = service_data
 
     @property
     def loaded_services(self) -> List:
         """List: list of the name of services of which the data has been loaded"""
-        return list(self._loaded_services_data.keys())
+        return list(self._all_service_data.keys())
 
-    @log_exec_time(LOADER_LOG)
-    def load_service_data_in_file(self, data_file_path, force_reload=False, service_name=None):
+    def load_service_data_from_file(self, data_file_path, service_name=None, force_reload=False):
         """load service data in a file
 
         Args:
             data_file_path (Path): path to the data file
-            force_reload (bool): reload the data file even if the service data has been loaded before, defaults to False
             service_name (str, optional): use the specified service name instead of file stem, defaults to None
+            force_reload (bool, optional): reload the data file even if the service data has been loaded before, defaults to False
 
         Returns:
-            Dict: loaded data
+            TosdrServiceData: loaded service data
 
         """
         if not service_name:
             service_name = data_file_path.stem
 
         if not data_file_path.exists():
-            raise LoadingServiceDataError(service_name, f'{data_file_path} does not exist')
+            raise ServiceDataLoadError(service_name, f'{data_file_path} does not exist')
 
-        if service_name not in self._loaded_services_data or force_reload:
-            # TODO: replace this one with proper load method to handle different versions of the TOSDR data file or
-            #  checking if the content is valid
+        if service_name not in self._all_service_data or force_reload:
             try:
                 with data_file_path.open() as f:
-                    data = json.load(f)
+                    json_content = json.load(f)
             except Exception as e:
-                raise LoadingServiceDataError(service_name, f'failed to load JSON file {data_file_path}: {e}')
-            if not data:
-                raise LoadingServiceDataError(service_name, f'service data in {data_file_path} is empty')
-            self._loaded_services_data[service_name] = data
-            return data
+                raise ServiceDataLoadError(service_name, f'failed to load JSON file {data_file_path}: {e}')
+            if not json_content:
+                raise ServiceDataLoadError(service_name, f'service data in {data_file_path} is empty')
+            service_data = TosdrServiceData.from_tosdr_data_file_json_content(service_name, json_content)
+            self.add_service_data(service_name, service_data, force_reload)
+            return service_data
         else:
-            LOADER_LOG.debug(f'`{service_name}` data existed')
-            return self._loaded_services_data[service_name]
+            LOADER_LOG.debug(f'`{service_name}` data existed and not force reload')
+            return self._all_service_data[service_name]
 
     @log_exec_time(LOADER_LOG)
-    def load_all_services_data_in_folder(self, folder_path: Path):
-        LOADER_LOG.debug(f"loading TosDR services data from {folder_path}...")
-        for file_path in folder_path.glob("**/*.json"):
+    def load_all_services_file_in_folder(self, data_folder: Path):
+        if not data_folder.exists():
+            raise FileExistsError(f"service data folder {data_folder} not found")
+        LOADER_LOG.debug(f"loading TosDR services data from {data_folder}...")
+        for file_path in data_folder.glob("**/*.json"):
             try:
-                self.load_service_data_in_file(file_path)
+                self.load_service_data_from_file(file_path)
                 LOADER_LOG.debug(f"loaded {file_path}")
             except Exception as e:
                 LOADER_LOG.error(f"failed to load file {file_path} : {e}")
                 continue
-        LOADER_LOG.info(f"loaded {len(self._loaded_services_data)} tosdr services data file")
+        LOADER_LOG.info(f"loaded {len(self._all_service_data)} tosdr services data file")
 
-    def service_data(self, service_name: str):
-        if service_name not in self._loaded_services_data:
+    def service_data(self, service_name: str) -> 'TosdrServiceData':
+        if service_name not in self._all_service_data:
             raise ValueError(f'`{service_name}` data not found. Please load the service data first')
-        return self._loaded_services_data.get(service_name)
+        return self._all_service_data[service_name]
 
 
-def service_quote_text_and_summary(service_data, remove_html_tags = True):
-    """
-
-    Args:
-        service_data (Dict): the loaded service data
-
-    Returns:
-        List[Tuple[Optional[str], Optional[str]]]: a list of tuples of quote text and the corresponding summary
-
-    """
-    service_point_data = service_data.get('pointsData')
-    if not service_point_data:
-        raise RuntimeError(f'`{service_data}` pointsData not found')
-
-    def get_quote_text(point_datum):
-        quote_text = point_datum.get('quoteText')
-        if remove_html_tags:
-            soup = BeautifulSoup(quote_text, 'lxml')
-            quote_text = soup.text
-        return quote_text
-
-    return [(get_quote_text(point_datum), point_datum.get('title')) for point_datum in service_point_data.values()]
-
-
-def service_urls(service_data):
-    """
+@dataclass
+class TosdrServiceSummaryCase:
+    """Data class for a tosdr service `pointsData` but only for cases that summarize a quoted text
 
     Args:
-        service_data (Dict): the loaded service data
-
-    Returns:
-        List[str]: list of urls of a service
-
+        point_id (str): the id of the point
+        quote_document (str): the name of the document of the service that was quoted
+        quote_text (str): the quoted text of the document
+        summary (str): the tosdr summary (`tosdr.case` by default, `title` if case does not exist)
+        point (str): the severity (neutral, bad, good)
     """
-    return service_data.get('urls')
+    point_id: str
+    quote_document: str
+    quote_text: str
+    summary: str
+    rating: str
 
+    @classmethod
+    def from_tosdr_point_data(cls, point_data):
+        quote_doc = point_data.get('quoteDoc')
+        if not quote_doc:
+            raise KeyError('`quoteDoc` field not found, point data is not a summary case')
+        # remove html tag in quote text
+        quote_text = BeautifulSoup(point_data['quoteText'], 'lxml').text
+        return cls(
+            point_data['id'],
+            quote_doc,
+            quote_text,
+            point_data['tosdr']['case'],
+            point_data['tosdr']['point']
+        )
+
+
+@dataclass
+class TosdrServiceData:
+    """Data class for tosdr service data from tosdr.org API
+
+    Args:
+        service_name (str): the name of the service
+        documents (List[str]): list of documents of a service
+        summary_cases (List[TosdrServiceSummaryCase): list of summary data
+    """
+    service_name: str
+    documents: List[str]
+    summary_cases: List[TosdrServiceSummaryCase]
+
+    @classmethod
+    def from_tosdr_data_file_json_content(cls, service_name, json_content):
+        points_data = json_content.get('pointsData')
+        if not points_data:
+            raise ServiceDataLoadError('the service has no pointsData')
+
+        summary_cases = []
+        for case in points_data.values():
+            try:
+                case = TosdrServiceSummaryCase.from_tosdr_point_data(case)
+                summary_cases.append(case)
+            except Exception as e:
+                LOADER_LOG.error(f"failed to load {service_name} point data {case['id']}: {e}")
+
+        # Not using the `links` field since it's often missing and not updated
+        documents = list({case.quote_document for case in summary_cases})
+        return cls(service_name, documents, summary_cases)
+
+    @property
+    def all_quote_texts_and_summaries(self) -> List[Tuple[str, str]]:
+        """
+
+        Returns:
+            List[Tuple[str, str]]: a list of tuples of quote text and the corresponding summary
+        """
+        return [(case.quote_text, case.summary) for case in self.summary_cases]
--- core/tests/data_handler/test_tosdr_data_handler.py
+++ core/tests/data_handler/test_tosdr_data_handler.py
@@ -1,5 +1,5 @@
 from core.tests.cases import CompareTestCase
-from core.data_handler.tosdr import TosdrDataLoader, service_quote_text_and_summary, service_urls
+from core.data_handler.tosdr import TosdrDataLoader
 
 
 class TestTosdrDataLoader(CompareTestCase):
@@ -10,23 +10,23 @@ class TestTosdrDataLoader(CompareTestCase):
         cls.services_input_folder = cls.input_folder.parent.parent / 'tosdr_services'
 
     def load_all_services_in_folder_step(self):
-        self.loader.load_all_services_data_in_folder(self.services_input_folder)
+        self.loader.load_all_services_file_in_folder(self.services_input_folder)
         self.assertCountEqual(self.loader.loaded_services, ['github', 'nvidia'])
 
     def force_reload_step(self):
         ori_github_data = self.loader.service_data('github')
-        self.loader.load_service_data_in_file(
+        self.loader.load_service_data_from_file(
             self.services_input_folder / 'nvidia.json', service_name='github', force_reload=True
         )
-        self.assertDictEqual(self.loader.service_data('nvidia'), self.loader.service_data('github'))
-        self.loader.load_service_data_in_file(
+        self.assertEqual(self.loader.service_data('nvidia').summary_cases, self.loader.service_data('github').summary_cases)
+        self.loader.load_service_data_from_file(
             self.services_input_folder / 'github.json', force_reload=True
         )
-        self.assertDictEqual(ori_github_data, self.loader.service_data('github'))
+        self.assertEqual(ori_github_data, self.loader.service_data('github'))
 
     def get_quote_text_and_summary_step(self):
         self.assertCountEqual(
-            service_quote_text_and_summary(self.loader.service_data('nvidia')),
+            self.loader.service_data('nvidia').all_quote_texts_and_summaries,
             [('Right to access. You can see what data we have collected whenever you '
               'want.\n\n'
               'Right to take your data. The data is yours.\n'
@@ -36,11 +36,7 @@ class TestTosdrDataLoader(CompareTestCase):
              ('We never sell your data.', 'This service does not sell your personal data')]
         )
 
-    def get_urls_step(self):
-        self.assertCountEqual(service_urls((self.loader.service_data('nvidia'))), ["nvidia.com", "nvidia.de"])
-
     def test_step_by_step(self):
         self.load_all_services_in_folder_step()
         self.force_reload_step()
         self.get_quote_text_and_summary_step()
-        self.get_urls_step()
