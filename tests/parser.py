c5183fcdb212b173a51f9abf99bded6a6218a2e9
Tri Nguyen <tringuyenminh23@gmail.com>
2023-11-06 18:12:59 +0200

    Refactor/project structure (#6)
    * init pyproject.toml
    
    * add poetry
    
    * add pre-commit hooks
    
    * refactor core dir

--- tests/parser.py
+++ tests/parser.py
@@ -7,8 +7,8 @@ import time
 import unittest
 from pathlib import Path
 
-from .results import TestResultLogMetrics, TestResultCompareFileMeld
 from . import TEST_DIR_PATH
+from .results import TestResultCompareFileMeld, TestResultLogMetrics
 
 
 class TestParser(argparse.ArgumentParser):
@@ -31,7 +31,7 @@ class TestParser(argparse.ArgumentParser):
         suites_dir = set()
         while checking_dirs:
             checking_d = checking_dirs.pop()
-            sub_dirs = {d for d in checking_d.iterdir() if d.is_dir() and d.stem != '__pycache__'}
+            sub_dirs = {d for d in checking_d.iterdir() if d.is_dir() and d.stem != "__pycache__"}
             if not sub_dirs:
                 suites_dir.add(checking_d)
             else:
@@ -40,7 +40,7 @@ class TestParser(argparse.ArgumentParser):
         for d in suites_dir:
             tests = unittest.TestLoader().discover(str(d))
             if tests.countTestCases() > 0:
-                test_suites['.'.join(d.relative_to(starting_dir).parts)] = tests
+                test_suites[".".join(d.relative_to(starting_dir).parts)] = tests
         return test_suites
 
     def discover_test_methods(self, starting_dir: Path):
@@ -62,7 +62,7 @@ class TestParser(argparse.ArgumentParser):
         def test_case_gen(t_suite):
             for test in t_suite:
                 if self.is_not_suite(test):
-                    case = '.'.join(test.id().split('.')[:-1])
+                    case = ".".join(test.id().split(".")[:-1])
                     yield case
                 else:
                     for t in test_case_gen(test):
@@ -72,7 +72,7 @@ class TestParser(argparse.ArgumentParser):
 
     def __init__(self):
         super().__init__()
-        self.prog = 'tests'
+        self.prog = "tests"
         self.epilog = """example:
         tests all
         tests all -d path/to/testDir
@@ -82,51 +82,51 @@ class TestParser(argparse.ArgumentParser):
         tests case suiteName.caseName.caseClass.methodName
         """
         self.add_argument(
-            '-l', '--logDirPath', type=str, default='.logs',
-            help=f"Path to log dir containing debug log (default: .logs)"
+            "-l",
+            "--logDirPath",
+            type=str,
+            default=".logs",
+            help="Path to log dir containing debug log (default: .logs)",
         )
-        self.add_argument('-s', '--logStdout', action='store_true', help='log to stdout as well as to a log file')
-        self.add_argument(
-            '-q', '--quiet', action='store_true',
-            help=f"Set logging level as INFO (default: False)"
-        )
-        self.add_argument('--meld', action='store_true', help='Use meld to compare out and exp file (default False)')
+        self.add_argument("-s", "--logStdout", action="store_true", help="log to stdout as well as to a log file")
+        self.add_argument("-q", "--quiet", action="store_true", help="Set logging level as INFO (default: False)")
+        self.add_argument("--meld", action="store_true", help="Use meld to compare out and exp file (default False)")
         sub_parser = self.add_subparsers(
-            title='command', dest='command', metavar='<command>', parser_class=argparse.ArgumentParser
+            title="command", dest="command", metavar="<command>", parser_class=argparse.ArgumentParser
         )
         sub_parser.required = True
-        suite_parser = sub_parser.add_parser('suite', help='Run a test suite')
-        suite_parser.add_argument('suiteName', type=str, help=f"Name of the suite to be run")
-        case_parser = sub_parser.add_parser('case', help='Run a test case or method')
-        case_parser.add_argument('caseName', type=str,
-                                 help="Path to TestCase or method from the testDir separated by dot")
-        sub_parser.add_parser('all', help='Test all cases discovered from the testDir ')
-        list_parser = sub_parser.add_parser(
-            'list', help='List discovered test suites or cases or methods from the testDir'
+        suite_parser = sub_parser.add_parser("suite", help="Run a test suite")
+        suite_parser.add_argument("suiteName", type=str, help="Name of the suite to be run")
+        case_parser = sub_parser.add_parser("case", help="Run a test case or method")
+        case_parser.add_argument(
+            "caseName", type=str, help="Path to TestCase or method from the testDir separated by dot"
         )
-        list_parser.add_argument('level', type=str, choices=['suite', 'case', 'method'])
+        sub_parser.add_parser("all", help="Test all cases discovered from the testDir ")
+        list_parser = sub_parser.add_parser(
+            "list", help="List discovered test suites or cases or methods from the testDir"
+        )
+        list_parser.add_argument("level", type=str, choices=["suite", "case", "method"])
 
     def __call__(self):
         args = self.parse_args()
         log_dir = TEST_DIR_PATH.joinpath(args.logDirPath)
         log_dir.mkdir(exist_ok=True)
 
-        os.environ['METRICS_LOG_PATH'] = str(log_dir / 'test_metrics.log')
+        os.environ["METRICS_LOG_PATH"] = str(log_dir / "test_metrics.log")
         file_handler = logging.handlers.RotatingFileHandler(
-            filename=str(log_dir / 'test.log'), maxBytes=10 * 1024 * 1024, backupCount=5
+            filename=str(log_dir / "test.log"), maxBytes=10 * 1024 * 1024, backupCount=5
         )
         log_config_kwargs = {
-            'format': '%(asctime)s %(name)-20s %(levelname)-10s %(message)s',
-            'datefmt': "%Y-%m-%dT%H:%M:%S%z",
-            'handlers': [file_handler, logging.StreamHandler()]
-            if args.logStdout else [file_handler]
+            "format": "%(asctime)s %(name)-20s %(levelname)-10s %(message)s",
+            "datefmt": "%Y-%m-%dT%H:%M:%S%z",
+            "handlers": [file_handler, logging.StreamHandler()] if args.logStdout else [file_handler],
         }
 
         if args.quiet:
-            log_config_kwargs['level'] = logging.INFO
+            log_config_kwargs["level"] = logging.INFO
             verbosity = 1
         else:
-            log_config_kwargs['level'] = logging.DEBUG
+            log_config_kwargs["level"] = logging.DEBUG
             verbosity = 2
         logging.basicConfig(**log_config_kwargs)
         logging.Formatter.converter = time.gmtime
@@ -136,30 +136,30 @@ class TestParser(argparse.ArgumentParser):
 
         all_succeed = True
         test_suites = self.discover_test_suites(TEST_DIR_PATH)
-        if args.command == 'all':
+        if args.command == "all":
             results = [runner.run(test_suites[s_name]).wasSuccessful() for s_name in test_suites]
             all_succeed = all(results)
-        elif args.command == 'suite':
+        elif args.command == "suite":
             if args.suiteName in list(test_suites.keys()):
                 all_succeed = runner.run(test_suites[args.suiteName]).wasSuccessful()
             else:
                 self.error(f"Suite {args.suiteName} not found in {TEST_DIR_PATH}. Use `list` option to list suite")
-        elif args.command == 'case':
-            relative_to_root = '.'.join(TEST_DIR_PATH.relative_to(os.getcwd()).parts)
+        elif args.command == "case":
+            relative_to_root = ".".join(TEST_DIR_PATH.relative_to(os.getcwd()).parts)
             suite = unittest.defaultTestLoader.loadTestsFromName(f"{relative_to_root}.{args.caseName}")
             all_succeed = runner.run(suite).wasSuccessful()
-        elif args.command == 'list':
-            if args.level == 'suite':
+        elif args.command == "list":
+            if args.level == "suite":
                 names = list(test_suites.keys())
-            elif args.level == 'case':
+            elif args.level == "case":
                 names = self.discover_test_cases(TEST_DIR_PATH)
             else:
                 names = self.discover_test_methods(TEST_DIR_PATH)
             if not names:
                 sys.stdout.write(f"No {args.level} found in {TEST_DIR_PATH}")
             else:
-                sys.stdout.write('\n'.join(names))
-                sys.stdout.write('\n')
+                sys.stdout.write("\n".join(names))
+                sys.stdout.write("\n")
 
         if not all_succeed:
             sys.exit("Some tests failed")
--- tests/results.py
+++ tests/results.py
@@ -1,40 +1,44 @@
-import unittest
-import subprocess
-from pathlib import Path
 import os
+import subprocess
+import unittest
+from pathlib import Path
 
 
 class TestResultLogMetrics(unittest.TextTestResult):
     def __init__(self, stream, descriptions, verbosity):
         super().__init__(stream, descriptions, verbosity)
-        self.metrics_log_path = Path(os.environ['METRICS_LOG_PATH']) if os.environ.get('METRICS_LOG_PATH') else None
+        self.metrics_log_path = Path(os.environ["METRICS_LOG_PATH"]) if os.environ.get("METRICS_LOG_PATH") else None
 
     def addSuccess(self, test):
         super().addSuccess(test)
         if self.metrics_log_path:
-            with self.metrics_log_path.open(mode='a+') as f:
+            with self.metrics_log_path.open(mode="a+") as f:
                 f.write(f"{getattr(test, '_started_at', '')}, {test.id()}, success, {getattr(test, '_elapsed', '')},\n")
 
     def addFailure(self, test, err):
         super().addFailure(test, err)
         if self.metrics_log_path:
-            with self.metrics_log_path.open(mode='a+') as f:
-                f.write(f"{getattr(test, '_started_at', '')}, {test.id()}, fail, {getattr(test, '_elapsed', '')}, "
-                        f"{err}\n")
+            with self.metrics_log_path.open(mode="a+") as f:
+                f.write(
+                    f"{getattr(test, '_started_at', '')}, {test.id()}, fail, {getattr(test, '_elapsed', '')}, "
+                    f"{err}\n"
+                )
 
     def addError(self, test, err):
         super().addError(test, err)
         if self.metrics_log_path:
-            with self.metrics_log_path.open(mode='a+') as f:
-                f.write(f"{getattr(test, '_started_at', '')}, {test.id()}, error, {getattr(test, '_elapsed', '')}, "
-                        f"{err}\n")
+            with self.metrics_log_path.open(mode="a+") as f:
+                f.write(
+                    f"{getattr(test, '_started_at', '')}, {test.id()}, error, {getattr(test, '_elapsed', '')}, "
+                    f"{err}\n"
+                )
 
 
 class TestResultCompareFileMeld(TestResultLogMetrics):
     def addFailure(self, test, err):
         super().addFailure(test, err)
-        out_file_path = getattr(test, 'out_file_path')
-        exp_file_path = getattr(test, 'exp_file_path')
+        out_file_path = test.out_file_path
+        exp_file_path = test.exp_file_path
         if out_file_path and exp_file_path:
             if Path(out_file_path).exists() and Path(exp_file_path).exists():
                 cont = True
@@ -46,6 +50,6 @@ class TestResultCompareFileMeld(TestResultLogMetrics):
                     elif res == "c":
                         cont = False
                     elif res == "d":
-                        subprocess.run(['meld', str(out_file_path), str(exp_file_path)])
+                        subprocess.run(["meld", str(out_file_path), str(exp_file_path)])
                     else:
-                        print('must input d, c, or f')
+                        print("must input d, c, or f")
