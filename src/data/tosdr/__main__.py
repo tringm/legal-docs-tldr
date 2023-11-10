e39e07e164b243cc6cacce4aaeff2144de405622
tringm <tringuyenminh23@gmail.com>
2023-11-11 01:06:40 +0200

    fix: cli download all metadata

--- src/data/tosdr/__main__.py
+++ src/data/tosdr/__main__.py
@@ -1,11 +1,13 @@
-import pickle
 from pathlib import Path
 
 import click
 
+from src.utils.file import write_ndjson_gz_file
+from src.utils.paths import DATA_DIR_PATH
+
 from .client import Client
 
-DEFAULT_ALL_SERVICES_OUTPUT_FILE = "./data/tosdr/all_services.pkl"
+DEFAULT_ALL_SERVICES_METADATA_OUTPUT_FILE = (DATA_DIR_PATH / "tosdr" / "all_services_metadata.ndjson.gz").resolve()
 
 
 @click.group()
@@ -15,20 +17,18 @@ def cli() -> None:
 
 @cli.command()
 @click.option(
-    '-o',
-    '--output-file',
-    default=DEFAULT_ALL_SERVICES_OUTPUT_FILE,
-    type=click.Path(file_okay=True, dir_okay=False, writable=True, path_type=Path)
+    "-o",
+    "--output-file",
+    default=DEFAULT_ALL_SERVICES_METADATA_OUTPUT_FILE,
+    type=click.Path(file_okay=True, dir_okay=False, writable=True, path_type=Path),
 )
-def download_all_services(output_file: Path) -> None:
-    """
-    Download and save as pickled list[Service]
-    """
+def download_all_services_metadata(output_file: Path) -> None:
+    """Download all services metadata and save as ndjson file"""
     client = Client()
-    services = client.get_all_services()
-    with output_file.open(mode="wb") as f:
-        pickle.dump(services, f)
+    services_metadata = client.get_all_services_metadata()
+    services_metadata_ndjson = [serv.model_dump() for serv in services_metadata]
+    write_ndjson_gz_file(data=services_metadata_ndjson, output_file=output_file)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
