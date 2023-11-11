b8ada6f49f9107e8c715cc41f5084f3610c0b8f3
tringm <tringuyenminh23@gmail.com>
2023-11-11 09:53:31 +0200

    add utils write pydantic models to ndjson.gz

--- tests/utils/test_file_utils.py
+++ tests/utils/test_file_utils.py
@@ -2,13 +2,23 @@ from collections.abc import Generator
 from pathlib import Path
 
 import pytest
+from pydantic import BaseModel
 
-from src.utils.file import read_ndjson_gz_file, write_ndjson_gz_file
+from src.utils.file_utils import read_ndjson_gz, write_ndjson_gz, write_pydantic_models_ndjson_gz
+
+
+class SampleModel(BaseModel):
+    pos: int
+
+
+@pytest.fixture
+def sample_models() -> list[BaseModel]:
+    return [SampleModel(pos=1)]
 
 
 @pytest.fixture
 def sample_ndjson() -> list[dict]:
-    return [{"first": 1}, {"second": 2}]
+    return [{"pos": 1}]
 
 
 @pytest.fixture
@@ -18,8 +28,17 @@ def sample_output_ndjson_gz_file() -> Generator[Path, None, None]:
     fp.unlink()
 
 
-def test_write_read_ndjson_gz(sample_ndjson: list[dict], sample_output_ndjson_gz_file: Path) -> None:
-    write_ndjson_gz_file(data=sample_ndjson, output_file=sample_output_ndjson_gz_file)
+def test_write_ndjson_gz(sample_ndjson: list[dict], sample_output_ndjson_gz_file: Path) -> None:
+    write_ndjson_gz(data=sample_ndjson, output_file=sample_output_ndjson_gz_file)
     assert sample_output_ndjson_gz_file.exists(), f"Expected file {sample_output_ndjson_gz_file}"
-    data = read_ndjson_gz_file(input_path=sample_output_ndjson_gz_file)
+    data = read_ndjson_gz(input_path=sample_output_ndjson_gz_file)
+    assert sample_ndjson == data, "Expected data read from gz ndjson file to be the same"
+
+
+def test_write_pydantic_models_ndjson_gz(
+    sample_models: list[BaseModel], sample_ndjson: list[dict], sample_output_ndjson_gz_file: Path
+) -> None:
+    write_pydantic_models_ndjson_gz(models=sample_models, output_file=sample_output_ndjson_gz_file)
+    assert sample_output_ndjson_gz_file.exists(), f"Expected file {sample_output_ndjson_gz_file}"
+    data = read_ndjson_gz(input_path=sample_output_ndjson_gz_file)
     assert sample_ndjson == data, "Expected data read from gz ndjson file to be the same"
