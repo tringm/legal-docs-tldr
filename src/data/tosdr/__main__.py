a1f0aaa7ec6efa79944d1791ac3dffb2f80492cb
tringm <tringuyenminh23@gmail.com>
2023-11-11 10:09:38 +0200

    fix tosdr download metadata cli

--- src/data/tosdr/__main__.py
+++ src/data/tosdr/__main__.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import click
 
-from src.utils.file import write_ndjson_gz_file
+from src.utils.file_utils import write_pydantic_models_ndjson_gz
 from src.utils.paths import DATA_DIR_PATH
 
 from .client import Client
@@ -26,8 +26,7 @@ def download_all_services_metadata(output_file: Path) -> None:
     """Download all services metadata and save as ndjson file"""
     client = Client()
     services_metadata = client.get_all_services_metadata()
-    services_metadata_ndjson = [serv.model_dump() for serv in services_metadata]
-    write_ndjson_gz_file(data=services_metadata_ndjson, output_file=output_file)
+    write_pydantic_models_ndjson_gz(models=services_metadata, output_file=output_file)
 
 
 if __name__ == "__main__":
