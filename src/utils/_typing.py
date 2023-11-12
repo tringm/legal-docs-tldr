f27465bd72911ba0f3124924a4ff409157cbb145
Tri Nguyen <tringuyenminh23@gmail.com>
2023-11-12 19:35:58 +0200

    Fix/pipelines (#10)
    * add missing ndjson package
    
    * use github actions pipeline
    
    * delete outdated code

--- src/utils/_typing.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-from typing import IO, Union
-
-FilePath = Union[str, Path]
-Buffer = IO
-FilePathOrBuffer = Union[FilePath, Buffer]
--- tests/__init__.py
+++
@@ -1,3 +0,0 @@
-from pathlib import Path
-
-TEST_DIR_PATH = Path(__file__).parent
