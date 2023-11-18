630cd2bdf22b6c4b3fbc589e202d9ef0319a6e94
tringm <tringuyenminh23@gmail.com>
2023-11-18 23:46:00 +0200

    add download all cases CLI

--- src/data/tosdr/__main__.py
+++ src/data/tosdr/__main__.py
@@ -13,6 +13,7 @@ from .client import Client
 TOSDR_DATA_DIR = (DATA_DIR_PATH / "tosdr").resolve()
 DEFAULT_ALL_SERVICES_METADATA_OUTPUT_FILE = TOSDR_DATA_DIR / "all_services_metadata.ndjson.gz"
 DEFAULT_ALL_SERVICES_OUTPUT_FILE = TOSDR_DATA_DIR / "all_services.ndjson.gz"
+DEFAULT_ALL_CASES_OUTPUT_FILE = TOSDR_DATA_DIR / "all_cases.ndjson.gz"
 
 
 @click.group()
@@ -28,7 +29,7 @@ def cli() -> None:
     type=click.Path(file_okay=True, dir_okay=False, writable=True, path_type=Path),
 )
 def download_all_services_metadata(output_file: Path) -> None:
-    """Download all services metadata and save as gzipped ndjson file"""
+    """Download all services metadata to a gzipped ndjson file"""
     client = Client()
     services_metadata = client.get_all_services_metadata()
     write_pydantic_models_ndjson_gz(models=services_metadata, output_file=output_file)
@@ -47,7 +48,7 @@ def download_all_services_metadata(output_file: Path) -> None:
     type=click.Path(file_okay=True, dir_okay=False, writable=True, path_type=Path),
 )
 def download_all_services(metadata_file: Path, output_file: Path) -> None:
-    """Download all services save as gzipped ndjson file"""
+    """Download all services to a gzipped ndjson file"""
     services_metadata = read_ndjson_gz(input_path=metadata_file)
     services_ids = [ServiceMetadata.model_validate(serv).id for serv in services_metadata]
 
@@ -55,6 +56,20 @@ def download_all_services(metadata_file: Path, output_file: Path) -> None:
     client = Client()
     services = asyncio.run(client.async_get_services(services_ids=services_ids))
     write_pydantic_models_ndjson_gz(models=services, output_file=output_file)
+
+
+@cli.command()
+@click.option(
+    "-o",
+    "--output-file",
+    default=DEFAULT_ALL_CASES_OUTPUT_FILE,
+    type=click.Path(file_okay=True, dir_okay=False, writable=True, path_type=Path),
+)
+def download_all_cases(output_file: Path) -> None:
+    """Download all cases to a gzipped ndjson file"""
+    client = Client()
+    cases = client.get_all_cases()
+    write_pydantic_models_ndjson_gz(models=cases, output_file=output_file)
 
 
 if __name__ == "__main__":
