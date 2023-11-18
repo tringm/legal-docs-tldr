666377ca9362f56cb1bc58f872ce6060a0bd402d
tringm <tringuyenminh23@gmail.com>
2023-11-18 23:43:14 +0200

    refactor get services metadata

--- src/data/tosdr/client.py
+++ src/data/tosdr/client.py
@@ -129,37 +129,44 @@ class Client(BaseAPIClient):
         max_tries=10,
         giveup=lambda e: e.status != codes.too_many,
     )
-    async def async_get_services_metadata(self, session: ClientSession, page_index: int) -> list[ServiceMetadata]:
+    async def async_get_service_metadata_page(
+        self, session: ClientSession, page_index: int
+    ) -> GetServiceMetadataPageResponse:
         async with self.rate_limiter, self.async_request(
             session=session, api_op=self._build_get_service_metadata_op(page_index=page_index)
         ) as resp:
             logger.info(f"Getting service page {page_index}")
             json_resp = await resp.json(content_type=None)
-            return GetServiceMetadataPageResponse.model_validate(json_resp).services_metadata
+            return GetServiceMetadataPageResponse.model_validate(json_resp)
 
-    async def async_get_services_metadata_from_pages(self, page_indices: list[int]) -> list[ServiceMetadata]:
+    async def async_get_multiple_services_metadata_pages(
+        self, page_indices: list[int]
+    ) -> list[GetServiceMetadataPageResponse]:
         async with ClientSession(raise_for_status=True) as session:
             tasks = (
-                self.async_get_services_metadata(session=session, page_index=page_idx) for page_idx in page_indices
+                self.async_get_service_metadata_page(session=session, page_index=page_idx) for page_idx in page_indices
             )
 
             coro_returns = await asyncio.gather(*tasks, return_exceptions=True)
 
-            serv_metadata = []
+            pages = []
             for idx, coro_ret in enumerate(coro_returns):
                 if isinstance(coro_ret, Exception):
                     logger.error(f"Failed to query service page {page_indices[idx]}: {coro_ret}")
                 else:
-                    serv_metadata += coro_ret
-            return serv_metadata
+                    pages.append(coro_ret)
+            return pages
 
     def get_all_services_metadata(self) -> list[ServiceMetadata]:
         first_page = self.get_service_metadata_page(page_index=1)
         remaining_pages_indices = list(range(2, first_page.total_page_count + 1))
-        remaining_serv_metadata = asyncio.run(
-            self.async_get_services_metadata_from_pages(page_indices=remaining_pages_indices)
+        remaining_pages = asyncio.run(
+            self.async_get_multiple_services_metadata_pages(page_indices=remaining_pages_indices)
         )
-        return [*first_page.services_metadata, *remaining_serv_metadata]
+        return [
+            *first_page.services_metadata,
+            *(serv_meta for page in remaining_pages for serv_meta in page.services_metadata),
+        ]
 
     async def async_get_services(self, services_ids: list[int]) -> list[Service]:
         async with ClientSession(raise_for_status=True) as session:
